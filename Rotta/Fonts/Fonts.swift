8440e08beec683521fae63f031f2a5ce44d3d694
Isadora Ferreira Guerra <isadora.guerra@edu.pucrs.br>
2025-06-12 15:49:55 -0300

    fonts cleaned

--- Rotta/Fonts/Fonts.swift
+++ Rotta/Fonts/Fonts.swift
@@ -9,18 +9,9 @@ import UIKit
 
 struct Fonts {
     static let BodyRegular = UIFont(name: "SFPro-Regular", size: 17)
-//    static let BodyRegular22 = UIFont(name: "SFPro-Regular", size: 22)
-    
     static let BodyEmphasized = UIFont(name: "SFPro-Semibold", size: 17)
-//    static let BodyEmphasized22 = UIFont(name: "SFPro-Semibold", size: 22)
-    
     static let FootnoteRegular = UIFont(name: "SFPro-Regular", size: 13)
-//    static let FootnoteRegular18 = UIFont(name: "SFPro-Regular", size: 18)
-    
     static let FootnoteEmphasized = UIFont(name: "SFPro-Semibold", size: 13)
-//    static let FootnoteEmphasized18 = UIFont(name: "SFPro-SemiBold", size: 18)
-    
     static let WeekdayCalendar = UIFont(name: "SFPro-Medium", size: 12)
-//    static let WeekdayCalendar16 = UIFont(name: "SFPro-Medium", size: 16)
 
 }
