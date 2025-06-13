32b74edff460eb6ef0ca7601c3b0181721c938e0
Marcos <marcosraach.1@gmail.com>
2025-06-13 13:15:28 -0300

    feat: database entities and removed folder view

---
+++ Rotta/Models/Properties/Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  Rotta
+//
+//  Created by Marcos on 13/06/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
