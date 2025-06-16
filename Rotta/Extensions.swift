8421313711ff409cd835d7db20b33b6a73cfb965
Isadora Ferreira Guerra <isadora.guerra@edu.pucrs.br>
2025-06-16 15:24:47 -0300

    some assets

--- Rotta/Extensions.swift
+++ Rotta/Extensions.swift
@@ -15,8 +15,8 @@ extension UIView {
         gradientLayer.endPoint   = CGPoint(x: 0.5, y: 1.0)
 
         gradientLayer.colors = colors ?? [
-            (UIColor(named: "start") ?? .clear).withAlphaComponent(0.2).cgColor,
-            (UIColor(named: "end") ?? .black).withAlphaComponent(1.0).cgColor
+            (UIColor(named: "SprintFormula2") ?? .clear).withAlphaComponent(0.2).cgColor,
+            (UIColor(named: "RaceFormula2") ?? .black).withAlphaComponent(1.0).cgColor
         ]
         gradientLayer.locations = [0.4, 0.9]
         self.layer.insertSublayer(gradientLayer, at: 0)
